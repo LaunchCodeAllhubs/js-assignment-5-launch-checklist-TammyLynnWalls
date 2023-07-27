01a4cda88334e9f0d71d7c02ccc856ec0789d0e5
unknown <tammylwalls@gmail.com>
2023-07-27 15:33:39 -0500

    added validation and preventDefault

--- scriptHelper.js
+++ scriptHelper.js
@@ -1,4 +1,9 @@
 // Write your helper functions here!
+
+//api site https://handlers.education.launchcode.org/static/planets.json
+validateInput();
+//console.log(validateInput())
+
 require('isomorphic-fetch');
 
 function addDestinationInfo(document, name, diameter, star, distance, moons, imageUrl) {
@@ -17,10 +22,51 @@ function addDestinationInfo(document, name, diameter, star, distance, moons, ima
 }
 
 function validateInput(testInput) {
-   
+    window.addEventListener("load", function() {
+        let form = document.querySelector("form");
+        form.addEventListener("submit", function() {
+          let  pilotName = document.querySelector("input[name=pilotName]");
+          let copilotName =  document.querySelector("input[name=copilotName]");
+          let fuelLevel =  document.querySelector("input[name=fuelLevel]");
+          let cargoMass =  document.querySelector("input[name=cargoMass]");
+          
+
+          if (pilotName.value === "" || copilotName.value === "") {
+            alert("all fields are required!")
+            event.preventDefault();
+            return "Empty";
+          }
+
+            if (fuelLevel.value === ""|| cargoMass.value === "") {
+                alert("all fields are required!")
+                event.preventDefault();
+                return "Empty";
+                            
+          }
+          if (isNaN(fuelLevel.value)||isNaN(cargoMass.value)) {
+            alert("nums only for fuel and cargo");
+            event.preventDefault();
+            return "Not a Number";
+          } else{
+            //alert("yea")
+            // event.preventDefault();
+            return "Is a Number";
+            
+          }
+       
+            
+          
+        });
+      });
 }
 
+
 function formSubmission(document, list, pilot, copilot, fuelLevel, cargoLevel) {
+    validateInput(testInput);
+
+    //Using template literals, update the li elements pilotStatus and copilotStatus to include the pilot's name and the co-pilot's name.
+
+
    
 }
 
